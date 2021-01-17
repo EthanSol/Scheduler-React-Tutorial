6f40ee18790cb8e0e6347e4fa67a023bc9912aff
EthanSol <ethanisrsolomon@gmail.com>
2021-01-17 12:04:39 -0600

    Course Filtering

--- components/CourseList.js
+++ components/CourseList.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
-import Course from './Course'
+import Course from './Course';
+import TermSelector from './TermSelector';
+
+// Term filtering constants
+const termMap = { F: 'Fall', W: "Winter", S: "Spring" }
+const terms = Object.values(termMap)
+
+// Function to get a course's term
+const getCourseTerm = course => (
+    termMap[course.id.charAt(0)]
+);
+
 
 // Dynamic courselist component
-const CourseList = ({courses}) => (
-    <ScrollView>
-        <View style={styles.courseList}>
-        {courses.map(course => <Course key={course.id} course={course} />)}
-        </View>
-    </ScrollView>
-);
+const CourseList = ({courses}) => {
+    // Initialize filtering state
+    const [selectedTerm, setSelectedTerm] = useState('Spring');
+    const termCourses = courses.filter(course => getCourseTerm(course) === selectedTerm);
+
+    // Render
+    return (
+        <ScrollView>
+            <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
+            <View style={styles.courseList}>
+                {termCourses.map(course => <Course key={course.id} course={course} />) }
+            </View>
+        </ScrollView>
+    );
+};
 
 const styles = StyleSheet.create({
     courseList: {
