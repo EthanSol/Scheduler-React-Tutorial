b3befa7fcae1c7406ad5eec4dc479c839fc2bfdd
EthanSol <ethanisrsolomon@gmail.com>
2021-01-27 12:56:50 -0600

    Store on Cloud

--- screens/CourseEditScreen.js
+++ screens/CourseEditScreen.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import Form from '../Form';
 import * as Yup from 'yup';
 
+import { firebase } from '../firebase';
+
 // Course edit screen
 const CourseEditScreen = ({route}) => {
   const course = route.params.course;
+  const [submitError, setSubmitError] = useState('');
+
+  async function handleSubmit(values) {
+    const { id, meets, title } = values;
+    const course = { id, meets, title };
+    firebase.database().ref('courses').child(id).set(course).catch(error => {
+      setSubmitError(error.message);
+    });
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -18,6 +29,7 @@ const CourseEditScreen = ({route}) => {
             title: course.title,
           }}
           validationSchema={validationSchema}
+          onSubmit={values => handleSubmit(values)}
         >
           <Form.Field
             name="id"
@@ -37,6 +49,8 @@ const CourseEditScreen = ({route}) => {
             leftIcon="format-title"
             placeholder="Introduction to programming"
           />
+          <Form.Button title={"Update"}/>
+          {<Form.ErrorMessage error={submitError} visible={true} />}
         </Form>
       </ScrollView>
     </SafeAreaView>
